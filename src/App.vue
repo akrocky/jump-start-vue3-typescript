56d574b7a13cb7c17c53e381244dc7d036f4ba2c
akrocky <abul64604@gmail.com>
2022-12-28 23:17:56 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <job-list :jobs="jobs" :order="order"> </job-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+let age: string | number = "25";
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { JobList },
+  setup(props) {
+    // const state = reactive({
+    //   name: "Link",
+    //   age: 25 as string | number,
+    // });
+    // return {
+    //   ...toRefs(state),
+    // };
+    // const name = ref("Link");
+    // const age = ref<number | string>(25);
+    // return { name, age };
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return {
+      jobs,
+      order,
+      handleClick,
+    };
+  },
+
+  //#endregion
+  methods: {},
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
